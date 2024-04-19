e991ad848cca26cb374c1f4283e0aae673dcf6ee
laurenspehl <131399540+laurenspehl@users.noreply.github.com>
2024-04-19 10:22:22 -0400

    Add files via upload

--- Database.java
+++ Database.java
@@ -1,4 +1,29 @@
 
 public class Database {
 
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void insert(Seminar seminar) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void search(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void printhashtable() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void printblocks() {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
