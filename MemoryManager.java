985870a16286bc7fd2d08b816bcf124d32fe2f83
laurenspehl <131399540+laurenspehl@users.noreply.github.com>
2024-04-19 10:42:31 -0400

    Add files via upload

--- MemoryManager.java
+++ MemoryManager.java
@@ -12,7 +12,7 @@ public class MemoryManager {
 
     private byte[] memoryPool;
     private ArrayList<Block> freeList;
-    
+
     /**
      * Constructor for the memory manager, creates a memory pool of the
      * specified size
@@ -23,5 +23,67 @@ public class MemoryManager {
     public MemoryManager(int insize) {
         freeList = new ArrayList<Block>();
         memoryPool = new byte[insize];
+    }
+
+
+    /**
+     * Insert a record of a given size
+     * 
+     * @param space
+     *            The byte array containing the record
+     * @param size
+     *            The size of the byte array
+     * @return The position handle of the inserted record
+     */
+    public Handle insert(byte[] space, int size) {
+        return new Handle(0, 0);
+    }
+
+
+    /**
+     * Calculates the size of a record
+     * 
+     * @param theHandle
+     *            An object of the handle class
+     * @return the length of the record associated with theHandle
+     */
+    public int length(Handle theHandle) {
+        return 0;
+    }
+
+
+    /**
+     * Free a block at the position specified by theHandle and merge the
+     * adjacent free blocks
+     * 
+     * @param theHandle
+     *            The handle that identifies the block to remove
+     */
+    public void remove(Handle theHandle) {
+
+    }
+
+
+    /**
+     * Return the record with handle posHandle, up to size bytes into space
+     * 
+     * @param space
+     *            The byte array to copy the contents of the record into
+     * @param theHandle
+     *            The handle of the record to copy
+     * @param size
+     *            Number of bytes of the record to return
+     * @return the number of bytes actually copied into space
+     */
+    public int get(byte[] space, Handle theHandle, int size) {
+        return 0;
+    }
+
+
+    /**
+     * Dump a printout of the freeblock list
+     */
+    public void dump() {
+
     }
 }
